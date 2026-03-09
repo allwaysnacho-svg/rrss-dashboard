53df8ba1048c54fa16d2aa226ede2bfac76a40d5
Tu Nombre <tu@email.com>
2026-03-09 19:16:03 +0100

    Fix prisma client export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as any
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient()
+  new PrismaClient({
+    log: ['query'],
+  })
 
-if (process.env.NODE_ENV !== 'production')
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
